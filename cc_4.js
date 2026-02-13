1b6d4443182301a66c6420655b39d6b437375235
Asce260901 <ajm55@usf.edu>
2026-02-12 19:22:00 -0500

    New approach to while loop, addid for in loop for all and then for one product

--- cc_4.js
+++ cc_4.js
@@ -1,4 +1,3 @@
-//products
 let products = [
     {sku:"001", pName: "BT Earbuds", category: "electronics", price: 49.99, stock: 42},
     {sku:"002", pName: "Phone Charger", category: "electronics", price: 24.18, stock: 65},
@@ -12,36 +11,9 @@ let products = [
     {sku:"010", pName: "Soccer ball", category: "sport", price: 35.99, stock: 91}
 ];
 
- //Orders and customer types (loy) loyal, (stu) student, (vet) veteran, (reg) regular
-let custOrders = [
-        {orderId:"001", customerType:"loy", cart: [{sku:"001", qty: 20}, {sku:"002", qty: 50}]},
-        {orderId:"002", customerType:"stu", cart: [{sku:"003", qty: 36}, {sku:"004", qty: 15}]},
-        {orderId:"003", customerType:"vet", cart: [{sku:"005", qty: 23}, {sku:"006", qty: 22}]},
-        {orderId:"004", customerType:"stu", cart: [{sku:"007", qty: 12}, {sku:"008", qty: 32}]},
-        {orderId:"005", customerType:"reg", cart: [{sku:"009", qty: 6}, {sku:"010", qty: 71}]}
- ];
-
- //variable for while loop condition
-let customQty = 1;
-let i = 0;
-
- //while loop
- while (customQty <=3 && i < custOrders.lenght){
-    for (const item of order.cart) {
-        let product = products[item.i];
-
-        let subtotal = 0;
-
-        let total = subtotal * (1-disRateTotal);
-
-        console.log(`Order ${custOrders.orderId} \nSubtotal: $${subtotal.toFixed(2)}. \nTotal: $${total.toFixed(2)}`);
-
-        customQty++;
-        i++;
-    }
-};
 //discount rate by category
 for (const product of products) {
+    let disRate = 0;
     switch (product.category) {
         case "electronics":
             disRate = 0.20; //20%
@@ -61,17 +33,67 @@ for (const product of products) {
     }
         let disPrice = product.price * (1-disRate);
         let discount = disRate * 100;
-        console.log(`Price for ${product.pName} in the category of ${product.category} is $${disPrice.toFixed(2)} after a discount of ${discount}% off , original price was $${product.price.toFixed(2)}`)
+        console.log(`Price for ${product.pName} is $${disPrice.toFixed(2)} after a discount of ${discount}% off , original price was $${product.price.toFixed(2)}`)
+        product.disPrice = disPrice.toFixed(2);
+        product.disRate = disRate.toFixed(2);
     };
+
+
+    //orders and customer types (loy) loyal, (stu) student, (vet) veteran, (reg) regular
+    let custOrders = [
+        {orderId:"001", customerType:"loy", cart: [{sku:"001", qty: 20}, {sku:"002", qty: 50}]},
+        {orderId:"002", customerType:"stu", cart: [{sku:"003", qty: 36}, {sku:"004", qty: 15}]},
+        {orderId:"003", customerType:"vet", cart: [{sku:"005", qty: 23}, {sku:"006", qty: 22}]},
+        {orderId:"004", customerType:"stu", cart: [{sku:"007", qty: 12}, {sku:"008", qty: 32}]},
+        {orderId:"005", customerType:"reg", cart: [{sku:"009", qty: 6}, {sku:"010", qty: 71}]}  
+    ];
 
     //dicount rate for customer types
-       if (custOrders.customerType === "loy") {
-        disRateTotal = 0.05; //5%
-    } else if (custOrders.customerType === "stu"){
-        disRateTotal = 0.10; //10%
-    } else if (custOrders.customerType === "vet"){
-        disRateTotal = 0.15; //15&
+    for (const order of custOrders) {
+       if (order.customerType === "loy") {
+        order.disRateTotal = 0.05; //5%
+    } else if (order.customerType === "stu"){
+        order.disRateTotal = 0.10; //10%
+    } else if (order.customerType === "vet"){
+        order.disRateTotal = 0.15; //15&
     } else {
-        disRateTotal = 0; //0%
+        order.disRateTotal = 0; //0%
+    }
     };
 
+    //variables for while loop
+    let i = 0; //order index
+    let n = 0; // customer number
+    let p = 0; // cart position
+    let subtotal = 0;
+    
+    //while loop
+    while (n <= 3 && i < custOrders.length){
+        let order = custOrders[i];
+        
+    while (p < order.cart.length){
+        let item = order.cart[p];
+        let product = products[(i*2)+p];
+
+        subtotal += product.disPrice*item.qty;
+        product.stock -= item.qty;
+
+        p++;
+    }
+        let total = subtotal *(1 - order.disRateTotal);
+        console.log(`Order ${order.orderId} processed: \nSubtotal: $${subtotal.toFixed(2)}. \nTotal: $${total.toFixed(2)}.`);
+        n++;
+        i++;
+    };
+
+    //For in loop for all products
+   // products.forEach((product) => {
+   //     for (const key in product) {
+   //         console.log(`${key}: ${product[key]}`) 
+   //     }     
+  //  });
+
+  // For in loop for a single product
+  for (const key in products[2]) {
+    console.log(`${key}: ${products[2][key]}`)
+  };
